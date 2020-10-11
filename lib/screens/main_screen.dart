a5a17358936d096d832c9e4ee544a5e21b4bf22c
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-10-11 23:01:20 +0545

    Changes: Search Bar COntainer Design WIP.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -33,6 +32,68 @@ class _MainPageState extends State<MainPage> {
               _controller.complete(controller);
               mapController = controller;
             },
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 240,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 15.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(07, 0.7),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Text("Nice to see you!", style: TextStyle(fontSize: 10)),
+                    Text(
+                      "Where are you going?",
+                      style: TextStyle(fontSize: 18, fontFamily: 'Brand-bold'),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: [BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 5.0,
+                          spreadRadius: 0.5,
+                          offset: Offset(
+                            0.7, 0.7
+                          )
+                        )]
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.search, color: Colors.blueAccent),
+                            SizedBox(width: 10),
+                            Text('Search Destination')
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
